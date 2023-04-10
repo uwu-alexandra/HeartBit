0ce434cce6cb3f5ca8f98effd40f3e0dcb241eb4
RobertPALAMIDA <palamidarobertadrian@gmail.com>
2023-04-10 15:45:20 +0300

    Logare

---
+++ MOB_Login/app/src/main/java/com/example/proiectingineriaprogramarii/Register.java
@@ -0,0 +1,98 @@
+package com.example.proiectingineriaprogramarii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class Register extends AppCompatActivity {
+
+    TextInputEditText editTextCnp,editTextPassword;
+    Button registerBtn;
+   // FireBaseAuth mAuth;
+    ProgressBar progressBar;
+    TextView textView;
+
+    /*
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+             Intent intent=new Intent(getApplicationContext(), MainActivity.class);
+             startActivity(intent);
+             finish();
+        }
+    }
+    */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        editTextCnp=findViewById(R.id.cnp);
+        editTextPassword=findViewById(R.id.password);
+        registerBtn=findViewById(R.id.registerBtn);
+       // mAuth=FirebaseAuth.getInstance();
+        progressBar=findViewById(R.id.progressBar);
+        textView=findViewById(R.id.loginNow);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String cnp,password;
+                cnp=editTextCnp.getText().toString();
+                password=editTextPassword.getText().toString();
+
+                if(TextUtils.isEmpty(cnp))
+                {
+                    Toast.makeText(Register.this, "Enter cnp", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password))
+                {
+                    Toast.makeText(Register.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+/*
+                mAuth.createUserWithEmailAndPassword(cnp, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Register.this, "Account created",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(Register.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+                        */
+
+            }
+        });
+    }
+}

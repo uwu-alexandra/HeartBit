d46c975fbcac36143b8fab4d4d375158c8c70f84
RobertPALAMIDA <palamidarobertadrian@gmail.com>
2023-04-11 16:53:18 +0300

    Meniu

--- MOBILE/HeartBit_mobile/src/main/java/com/heartbit_mobile/MainActivity.java
+++ MOBILE/HeartBit_mobile/src/main/java/com/heartbit_mobile/MainActivity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.heartbit_mobile.databinding.ActivityMainBinding;
+import com.heartbit_mobile.ui.home.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +36,32 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-    }
 
+        binding.navView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId())
+            {
+                case R.id.navigation_support:
+                        replaceFragment(new SupportFragment());
+                    break;
+                case R.id.navigation_calendar:
+                    break;
+                case R.id.navigation_home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.navigation_dashboard:
+                    break;
+                case R.id.navigation_settings:
+                    break;
+            }
+
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment)
+    {
+    FragmentManager fragmentManager=getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+    fragmentTransaction.replace(R.id.frame_layout,fragment);
+    fragmentTransaction.commit();
+    }
 }

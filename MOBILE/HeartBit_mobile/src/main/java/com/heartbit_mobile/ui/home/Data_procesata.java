f753923453db63148850d40522c3d66045b7808c
RobertPALAMIDA <palamidarobertadrian@gmail.com>
2023-05-05 20:39:53 +0300

    Clasa Procesare update

--- MOBILE/HeartBit_mobile/src/main/java/com/heartbit_mobile/ui/home/Data_procesata.java
+++ MOBILE/HeartBit_mobile/src/main/java/com/heartbit_mobile/ui/home/Data_procesata.java
@@ -5,27 +5,49 @@ import com.google.firebase.database.PropertyName;
 import java.util.Date;
 
 public class Data_procesata {
-    @PropertyName("Tip_data")
-    private String tip_data;
+    @PropertyName("Denumire")
+    private String denumire;
     @PropertyName("Time_stamp")
     private Date time_stamp;
+    @PropertyName("Identificator")
+    private String identificator;
+    @PropertyName("Alerta")
+    private boolean alerta;
+    @PropertyName("Valoare")
+    private float valoare;
 
     public Data_procesata() {
     }
 
-    public Data_procesata(String tip_data, Date time_stamp) {
-        this.tip_data = tip_data;
+    public Data_procesata(String identificator, Date time_stamp, float valoare) {
+        this.identificator = identificator;
+        switch (identificator) {
+            case "EKG":
+                denumire = "EKG sensor";
+                break;
+            case "UMD":
+                denumire = "Umidity sensor";
+                break;
+            case "TEMP":
+                denumire = "Temperature sensor";
+                break;
+            case "PULS":
+                denumire = "Pulse sensor";
+                break;
+        }
         this.time_stamp = time_stamp;
+        this.alerta = false;
+        this.valoare = valoare;
     }
 
-    @PropertyName("Tip_data")
-    public String getTip_data() {
-        return tip_data;
+    @PropertyName("Denumire")
+    public String getDenumire() {
+        return denumire;
     }
 
-    @PropertyName("Tip_data")
-    public void setTip_data(String tip_data) {
-        this.tip_data = tip_data;
+    @PropertyName("Denumire")
+    public void setDenumire(String denumire) {
+        this.denumire = denumire;
     }
 
     @PropertyName("Time_stamp")
@@ -36,5 +58,35 @@ public class Data_procesata {
     @PropertyName("Time_stamp")
     public void setTime_stamp(Date time_stamp) {
         this.time_stamp = time_stamp;
+    }
+
+    @PropertyName("Identificator")
+    public String getIdentificator() {
+        return identificator;
+    }
+
+    @PropertyName("Identificator")
+    public void setIdentificator(String identificator) {
+        this.identificator = identificator;
+    }
+
+    @PropertyName("Alerta")
+    public boolean getAlerta() {
+        return alerta;
+    }
+
+    @PropertyName("Denumire")
+    public void setAlerta(boolean alerta) {
+        this.alerta = alerta;
+    }
+
+    @PropertyName("Valoare")
+    public void setValoare(float valoare) {
+        this.valoare = valoare;
+    }
+
+    @PropertyName("Valoare")
+    public float getValoare() {
+        return valoare;
     }
 }
--- MOBILE/HeartBit_mobile/src/main/java/com/heartbit_mobile/ui/home/ProcesareThread.java
+++ MOBILE/HeartBit_mobile/src/main/java/com/heartbit_mobile/ui/home/ProcesareThread.java
@@ -6,14 +6,14 @@ import java.sql.Date;
 import java.util.Queue;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class ProcesareThread extends Thread{
+public class ProcesareThread extends Thread {
     private Queue<String> bufferQueue;
     private ReentrantLock bufferLock = new ReentrantLock();
     private boolean isProcessing;
 
-    public ProcesareThread(Queue<String>bufferQueue) {
-        this.bufferQueue=bufferQueue;
-        isProcessing=true;
+    public ProcesareThread(Queue<String> bufferQueue) {
+        this.bufferQueue = bufferQueue;
+        isProcessing = true;
     }
 
     public void stopProcessing() {
@@ -22,29 +22,26 @@ public class ProcesareThread extends Thread{
 
     public void run() {
         while (isProcessing) {
-            String data=null;
+            String data = null;
             bufferLock.lock();
-            if(!bufferQueue.isEmpty())
-            {
-                data=bufferQueue.poll();
+            if (!bufferQueue.isEmpty()) {
+                data = bufferQueue.poll();
             }
             bufferLock.unlock();
             if (data != null) {
-                // Procesare
+                //Procesare
                 //String split in tip_data si time_stamp
-                String procesare[]=data.split(";");
-                String tip_data=procesare[0];
-                Date time_stamp= Date.valueOf(procesare[1]);
-                Data_procesata dataProcesata=new Data_procesata(tip_data,time_stamp);
+                String procesare[] = data.split(";");
+                String identificator = procesare[0];
+                Date time_stamp = Date.valueOf(procesare[1]);
+                float valoare=Float.valueOf(procesare[2]);
+                Data_procesata dataProcesata = new Data_procesata(identificator, time_stamp,valoare);
 
                 //trimitere in cloud in functie de tipul de data
-                if(dataProcesata.getTip_data().compareTo("EKG")==0)
-                {
-                    //->trimitere in tabela cu EKG
-                }
-                else
-                {
-                    //->trimitere in tabela cu ceilalti senzori
+                if (dataProcesata.getIdentificator().compareTo("EKG") == 0) {
+                    // ->trimitere in tabela cu EKG
+                } else {
+                    // ->trimitere in tabela cu ceilalti senzori
                 }
             }
         }
